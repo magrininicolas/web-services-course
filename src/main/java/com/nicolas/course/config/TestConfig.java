511b122007c46fa7f2b48937eec25c46fa56ba20
Nicolas <magrininicolas@outlook.com>
2024-04-30 14:51:50 -0300

    Modified TestConfig

--- src/main/java/com/nicolas/course/config/TestConfig.java
+++ src/main/java/com/nicolas/course/config/TestConfig.java
@@ -1,9 +1,15 @@
 package com.nicolas.course.config;
 
+import java.time.Instant;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.nicolas.course.entities.Order;
+import com.nicolas.course.entities.User;
+import com.nicolas.course.repositories.OrderRepository;
 import com.nicolas.course.repositories.UserRepository;
 
 @Configuration
@@ -11,18 +17,24 @@ import com.nicolas.course.repositories.UserRepository;
 public class TestConfig implements CommandLineRunner {
 
     private UserRepository userRepository;
+    private OrderRepository orderRepository;
 
-    public TestConfig(UserRepository userRepository) {
+    public TestConfig(UserRepository userRepository, OrderRepository orderRepository) {
         this.userRepository = userRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        // User u1 = new User(null, "Nicolas", "nicolas@email.com", "11888888888",
-        // "nicolasnicolas");
-        // User u2 = new User(null, "Rodson", "rodson@email.com", "11999999999",
-        // "rodsonrodson");
+        User u1 = new User(null, "Nicolas", "nicolas@email.com", "11888888888",
+                "nicolasnicolas");
+        User u2 = new User(null, "Rodson", "rodson@email.com", "11999999999",
+                "rodsonrodson");
+        Order o1 = new Order(null, Instant.parse("2024-02-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2024-03-19T19:53:07Z"), u1);
+        Order o3 = new Order(null, Instant.parse("2024-01-20T19:53:07Z"), u2);
 
-        // userRepository.saveAll(List.of(u1, u2));
+        userRepository.saveAll(List.of(u1, u2));
+        orderRepository.saveAll(List.of(o1, o2, o3));
     }
 }
